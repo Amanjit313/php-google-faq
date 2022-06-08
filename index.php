322ac8a4a02337d08cde55b5a4d1d035d034a990
Amanjit313 <amanjitsingh313@gmail.com>
2022-06-08 15:53:14 +0200

    Footer - Complete

--- index.php
+++ index.php
@@ -105,6 +105,7 @@ $faq = [
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>PHP Google Faq</title>
   <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
 
@@ -118,6 +119,29 @@ $faq = [
   
     <?php endforeach; ?>
   </main>
+
+  <footer>
+    <div class="footer-container">
+
+      <div class="footer-link">
+        <span>Google</span>
+        <span>Termini su Google</span>
+        <span>Privacy</span>
+        <span>Termini</span>
+      </div>
+
+      <div class="footer-lang">
+        <i class="fa-solid fa-globe"></i>
+        <select name="lang" id="lang">
+          <option value="italian">Italiano</option>
+          <option value="english">English</option>
+          <option value="englishUK">English (UK)</option>
+          <option value="hindi">Hindi</option>
+        </select>
+      </div>
+
+    </div>
+  </footer>
 
 </body>
 </html>
